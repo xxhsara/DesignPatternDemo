a9634140453a47b2a49e619bccb5667fcd8cfa83
xianghua.xie <xianghua.xie@yhglobal.com>
2022-07-11 17:01:31 +0800

    feat: ObserverPattern Init

---
+++ DesignPattern/ObserverPattern/Observers/Lamp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Observers
+{
+    public class Lamp : AObserver
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Open the lamp");
+        }
+    }
+}
